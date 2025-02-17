278c527d3257a7cbe2a5f929d54d16befbf5d6c8
Habibur Rahaman <habibur@quantummethod.org.bd>
2025-02-17 22:53:01 +0600

    Updated settings py

--- event_management/settings.py
+++ event_management/settings.py
@@ -179,6 +179,6 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-FRONTEND_URL = 'http://127.0.0.1:8000'
+FRONTEND_URL = 'https://event-management-dc3f.onrender.com'
 
 LOGIN_URL = 'sign-in'
